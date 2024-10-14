098a73ef634829329dff5d561f0a13b9b690775b
DzmitryPresnakou <7745834@gmail.com>
2024-10-14 11:30:44 +0300

    added methods in all repositories

--- service/src/main/java/com/presnakov/hotelbooking/dao/OrderRepository.java
+++ service/src/main/java/com/presnakov/hotelbooking/dao/OrderRepository.java
@@ -1,11 +1,47 @@
 package com.presnakov.hotelbooking.dao;
 
 import com.presnakov.hotelbooking.entity.Order;
+import com.querydsl.jpa.impl.JPAQuery;
 import jakarta.persistence.EntityManager;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static com.presnakov.hotelbooking.entity.QHotel.hotel;
+import static com.presnakov.hotelbooking.entity.QOrder.order;
+import static com.presnakov.hotelbooking.entity.QRoom.room;
+import static com.presnakov.hotelbooking.entity.QUser.user;
 
 public class OrderRepository extends RepositoryBase<Integer, Order> {
 
     public OrderRepository(EntityManager entityManager) {
         super(Order.class, entityManager);
+    }
+
+    public List<Order> findOrderByUserEmail(String email) {
+        return new JPAQuery<Order>(getEntityManager())
+                .select(order)
+                .from(order)
+                .join(order.user, user)
+                .where(user.email.eq(email))
+                .fetch();
+    }
+
+    public List<Order> findAllOrdersByHotelName(String name) {
+        return new JPAQuery<Order>(getEntityManager())
+                .select(order)
+                .from(order)
+                .join(order.room, room)
+                .join(room.hotel, hotel)
+                .where(hotel.name.eq(name))
+                .fetch();
+    }
+
+    public List<Order> findOrdersByCheckInDate(LocalDate checkInDate) {
+        return new JPAQuery<Order>(getEntityManager())
+                .select(order)
+                .from(order)
+                .where(order.checkInDate.eq(checkInDate))
+                .fetch();
     }
 }
--- service/src/main/java/com/presnakov/hotelbooking/dao/RoomRepository.java
+++ service/src/main/java/com/presnakov/hotelbooking/dao/RoomRepository.java
@@ -1,11 +1,48 @@
 package com.presnakov.hotelbooking.dao;
 
+import com.presnakov.hotelbooking.dto.RoomFilter;
 import com.presnakov.hotelbooking.entity.Room;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.jpa.impl.JPAQuery;
 import jakarta.persistence.EntityManager;
+
+import java.util.List;
+
+import static com.presnakov.hotelbooking.entity.QHotel.hotel;
+import static com.presnakov.hotelbooking.entity.QOrder.order;
+import static com.presnakov.hotelbooking.entity.QRoom.room;
 
 public class RoomRepository extends RepositoryBase<Integer, Room> {
 
     public RoomRepository(EntityManager entityManager) {
         super(Room.class, entityManager);
+    }
+
+    public List<Room> findAllByHotelNameClassOccupancyPriceFreeDate(RoomFilter filter) {
+        return new JPAQuery<Room>(getEntityManager())
+                .select(room)
+                .from(order)
+                .join(order.room, room)
+                .join(room.hotel, hotel)
+                .where(getFirstPredicate(filter), getSecondPredicate(filter))
+                .fetch();
+    }
+
+    private static Predicate getSecondPredicate(RoomFilter filter) {
+        return QPredicate.builder()
+                .add(filter.getCheckOutDate(), order.checkInDate::after)
+                .add(filter.getCheckOutDate(), order.checkInDate::eq)
+                .add(filter.getCheckInDate(), order.checkOutDate::before)
+                .add(filter.getCheckInDate(), order.checkOutDate::eq)
+                .buildOr();
+    }
+
+    private static Predicate getFirstPredicate(RoomFilter filter) {
+        return QPredicate.builder()
+                .add(filter.getHotelName(), hotel.name::eq)
+                .add(filter.getOccupancy(), room.occupancy::eq)
+                .add(filter.getPricePerDay(), room.pricePerDay::eq)
+                .add(filter.getRoomClass(), room.roomClass::eq)
+                .buildAnd();
     }
 }
