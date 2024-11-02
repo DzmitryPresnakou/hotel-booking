dcab57c21bb1c88c3e86ca79b798c80050670e91
DzmitryPresnakou <7745834@gmail.com>
2024-11-02 17:25:56 +0300

    deleted configurations files

--- service/src/main/java/com/presnakov/hotelbooking/config/ApplicationConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.presnakov.hotelbooking.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration()
-public class ApplicationConfiguration {
-
-}
--- service/src/test/java/com/presnakov/hotelbooking/config/ApplicationTestConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.presnakov.hotelbooking.config;
-
-import org.springframework.context.annotation.Import;
-
-@Import(ApplicationConfiguration.class)
-public class ApplicationTestConfiguration {
-
-}
